cc4f63d04544657a315b7dd4adcb2b383f9223d3
joelgnanasekaram <gnanasej@sheridancollege.ca>
2021-04-10 18:32:46 -0400

    Updated the main Index Javascript file by implementing the code that specifies the port that the express server will run on , as well as the code that tells the app to listen for specificied port and start the express server in the Luck Be A LandLord Game Web API using Node, Express, MongoDB Mongoose, and Heroku Project

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const app = express();
 // configure express to accept data from the client as JSON format
 app.use(express.json())
 
+// specify the port that your server will run on
+const HTTP_PORT = process.env.PORT || 8080;
+
+// start the server and output a message if the server started successfully
+const onHttpStart = () => {
+    console.log(`Server has started and is listening on port ${HTTP_PORT}`)
+}
+
+app.listen(HTTP_PORT, onHttpStart);
+   
+
 
